395864b4f627082565844e78e277d28cc3fa91c2
Marvin Brouwer <marvin-brouwer@live.nl>
2016-01-17 09:56:03 +0100

    Created UI for image QR-Codes

--- Source/QR-Reader/Constants.ts
+++ Source/QR-Reader/Constants.ts
@@ -1,5 +1,28 @@
 ﻿class Constants{
     public static version = '1.0.0';
     // ReSharper disable once InconsistentNaming
-    public static agreedToTOA = `argreedToTOA/V-${Constants.version.split('.')[0]}`;
+    public static agreedToTOA = `argreedToTOA/V-1`; // update if TOA changes
+    public static pdfBase = '';
+    public static imageTypes = {
+        jpeg : ['FFD8'],
+        jpg : ['FFD8'],
+        png : ['89504E470D0A1A0A'],
+        gif : ['474946'],
+        bmp : ['424D'],
+        tiff : ['4949', '4D4D']
+    };
+    // https://gist.github.com/gruber/f7d2a569b3fb51d48a89
+    public static invalidUrls = [
+        'adclick.g.doubleclick.net', 'addthis.com', 'adgardener.com', 'adnxs.com',
+        'ads.pointroll.com', 'ads.shorttail.net', 'apture.com', 'bop.fm', 'cdn.taboolasyndication.com',
+        'chartbeat.com', 'chartbeat.net', 'd1.openx.org', 'doubleclick.net', 'doubleverify.com',
+        'exitjunction.com', 'fyre.co', 'getconnected.southwestwi-fi.com', 'googleadservices.com',
+        'gravity.com', 'grvcdn.com', 'imrworldwide.com', 'intellitxt.com', 'jetpackdigital.com',
+        'kontera.com', 'livefyre.com', 'luminate.com', 'meebo.com', 'moovmanage.com', 'outbrain.com',
+        'parsely.com', 'po.st', 'pointroll.com', 'pubmatic.com', 'quantserve.com', 's.ppjol.net',
+        'scorecardresearch.com', 'serving-sys.com', 'sharethis.com', 'snap.com', 'superclick.com',
+        'taboola.com', 'taboolasyndication.com', 'tynt.com', 'wibiya.com'
+    ];
+    https://commons.wikimedia.org/wiki/File:Transparent.gif
+    public static transparentGif = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 }
--- Source/QR-Reader/DataProcessors/IllustrationDataProcessor.ts
+++ Source/QR-Reader/DataProcessors/IllustrationDataProcessor.ts
@@ -2,7 +2,14 @@
     public dataType: DataType = DataType.Illustration;
 
     public initiate(data: string): void {
-        let sure = confirm(`Image`);
+        let actionManager = <ActionManager>ioc.Container.getCurrent().resolve<ActionManager>(ActionManager);
+        let imageType = (<RegExp><any>this.dataType).exec(data)[2];
+        let isValid = !!Constants.imageTypes[imageType];
+        let imageContainer = document.createElement('img');
+        imageContainer.src = isValid ? data : Constants.transparentGif;
+        imageContainer.className = 'illustration' + (isValid ? String() : ' invalid');
+        actionManager.showCallToAction(`${DataType[this.dataType]}: ${imageType}`, imageContainer,
+            isValid ? String() : `The image of type '${imageType}' is not supported!`);
     }
 
     // Leave these
