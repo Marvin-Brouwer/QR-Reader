73e1fa0f11e79f80270f0a6bacd375dc8ac19d81
Marvin Brouwer <marvin-brouwer@live.nl>
2016-01-17 12:36:19 +0100

    Added Email UI

--- Source/QR-Reader/DataProcessors/EmailDataProcessor.ts
+++ Source/QR-Reader/DataProcessors/EmailDataProcessor.ts
@@ -2,11 +2,13 @@
     public dataType: DataType = DataType.Email;
 
     public initiate(data: string): void {
+        let actionManager = <ActionManager>ioc.Container.getCurrent().resolve<ActionManager>(ActionManager);
         let email = data.replace(<any>DataType.Email, String());
         email = email.indexOf('?') < 0 ? email : email.split('?')[0];
-        if (confirm(`Send an email to ${email}?`))
-            UrlHelper.redirect(data);
-            
+        let linkContainer = document.createElement('a');
+        linkContainer.href = data;
+        linkContainer.className = 'textMessage';
+        actionManager.showCallToAction(`${DataType[DataType.Email]}: ${email}`, linkContainer);
     }
 
     // Leave these
